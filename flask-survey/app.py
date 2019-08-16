400dbad7c90252c8a71544efdf516ee4ca36ab63
Diana <diana@Dianas-MacBook-Pro.local>
2019-08-15 17:00:38 -0700

    completed up to step7

--- flask-survey/app.py
+++ flask-survey/app.py
@@ -1,35 +1,56 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
-
-responses = []
-question_number = 0
 
 
 @app.route("/", methods=["GET"])
 def initial_survey():
     '''Uploading the initial page'''
-
     return render_template("start.html",
                            title=satisfaction_survey.title,
                            mid_text=satisfaction_survey.instructions)
 
+@app.route("/start", methods=["POST", "GET"])
+def create_session():
+    responses = session["responses"]
+    return redirect("/questions")
 
-@app.route("/question/<question_number>", methods=["POST"])
-def route_questions(question_number):
-    if (question_number == len(satisfaction_survey.questions)):
+
+@app.route("/questions/<int:qid>", methods=["GET"])
+def route_questions(qid):
+    sat_surv = satisfaction_survey
+    if(len(session) != qid or qid > len(session)):
+        flash("Accessing an invalid question, you have been redirected")
+        return redirect(f"/questions/{len(session)}")
+    return render_template("question.html",
+        title=sat_surv.title,
+        mid_text=sat_surv.questions[qid].question,
+        selection=sat_surv.questions[qid].choices)
+
+
+@app.route("/questions")
+def questions_redirector(responses):
+    if (len(responses) == len(satisfaction_survey.questions)):
         return redirect("/thanks")
     else:
-        if(question_number > 0):
-            responses.append(request.form[''])
-        question_number += 1
-        return render_template("question.html",
-                           title=satisfaction_survey.title,
-                           mid_text=satisfaction_survey.question[question_number].question,
-                           selection=satisfaction_survey.question[question_number].choices)
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/answer", methods=["POST"])
+def get_answers(responses):
+    choice = request.form["answer"]
+    responses.append("choice")
+    session["responses"] = responses
+
+    return redirect(f"/questions")
+       
+@app.route("/thanks")
+def thanks():
+    return render_template("thanks.html")
